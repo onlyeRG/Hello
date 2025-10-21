ff085f776bcd074b384ada91bca32086e9addeac
onlyeRG <hindinarutoshippuden@gmail.com>
2025-10-21 14:55:29 +0530

    Configure Flask app to use custom host and port

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from config import PORT, HOST
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +9,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run()
-  
+    app.run(host=HOST, port=PORT, debug=False)
